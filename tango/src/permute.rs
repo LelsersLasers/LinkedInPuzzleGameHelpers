ad04275e096f565bc218c6474505ed67fc8d6719
Lord-Lelsers <48894200+LelsersLasers@users.noreply.github.com>
2024-12-29 13:14:46 -0800

    Improving comments

--- tango/src/permute.rs
+++ tango/src/permute.rs
@@ -2,17 +2,15 @@ use itertools::Itertools;
 use macroquad::rand::ChooseRandom;
 
 pub fn permutations_with_equal_ones_and_twos(x: usize) -> impl Iterator<Item = Vec<u8>> {
-    assert!(x % 2 == 0, "X must be even");
-    
-    // Generate the initial vector with equal numbers of 1s and 2s
     let half = x / 2;
     let mut base = vec![1; half].into_iter()
         .chain(vec![2; half])
         .collect::<Vec<u8>>();
+    
+    // Shuffle to make unique not have to skip larger chuncks at once
     base.shuffle();
     base.shuffle();
     
-    // Use itertools::permutations to generate all permutations
     // base.into_iter().permutations(x)
     base.into_iter().permutations(x).unique()
 }
