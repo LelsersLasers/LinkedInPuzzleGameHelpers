9cc9c5b709f01b8b0a3537e591891e205ddce929
Lord-Lelsers <48894200+LelsersLasers@users.noreply.github.com>
2024-12-29 09:54:43 -0800

    Simple squares system

--- tango/src/main.rs
+++ tango/src/main.rs
@@ -1,5 +1,26 @@
 use macroquad::prelude as mq;
 
+
+
+
+#[derive(Clone, Copy)]
+enum Square {
+    Empty,
+    Sun,
+    Moon,
+}
+
+
+fn xy_to_index(x: u32, y: u32, width: u32) -> usize {
+    (y * width + x) as usize
+}
+
+
+
+
+
+
+
 fn window_conf() -> mq::Conf {
     mq::Conf {
         window_title: "Tango Solver".to_owned(),
@@ -12,7 +33,8 @@ fn window_conf() -> mq::Conf {
 
 #[macroquad::main(window_conf)]
 async fn main() {
-    let mut squares: u32 = 6;
+    let mut squares_count: u32 = 6;
+    let mut squares: Vec<Square> = Vec::with_capacity((squares_count * squares_count) as usize);
 
     loop {
         // Clear background
@@ -20,7 +42,7 @@ async fn main() {
 
         // Draw squares counter
         mq::draw_text(
-            &format!("Squares:{}", squares),
+            &format!("Squares:{}", squares_count),
             8.0,
             20.0,
             24.0,
@@ -28,24 +50,74 @@ async fn main() {
         );
 
         // Draw increment button
-        mq::draw_rectangle(110.0, 6.0, 16.0, 16.0, mq::BLACK);
-        mq::draw_text("+", 113.0, 19.0, 24.0, mq::WHITE);
+        mq::draw_rectangle(116.0, 6.0, 16.0, 16.0, mq::BLACK);
+        mq::draw_text("+", 119.0, 19.0, 24.0, mq::WHITE);
 
         // Draw decrement button
-        mq::draw_rectangle(130.0, 6.0, 16.0, 16.0, mq::BLACK);
-        mq::draw_text("-", 133.0, 19.0, 24.0, mq::WHITE);
+        mq::draw_rectangle(136.0, 6.0, 16.0, 16.0, mq::BLACK);
+        mq::draw_text("-", 139.0, 19.0, 24.0, mq::WHITE);
 
         // Check if increment/decrement buttons are pressed
         if mq::is_mouse_button_pressed(mq::MouseButton::Left) {
             let mouse_pos = mq::mouse_position();
-            if mouse_pos.0 > 110.0 && mouse_pos.0 < 126.0 && mouse_pos.1 > 6.0 && mouse_pos.1 < 22.0 {
-                squares += 2;
+            if mouse_pos.0 > 116.0 && mouse_pos.0 < 132.0 && mouse_pos.1 > 6.0 && mouse_pos.1 < 22.0 {
+                squares_count = (squares_count + 2).min(8);
+                squares = Vec::with_capacity((squares_count * squares_count) as usize);
             }
-            if mouse_pos.0 > 130.0 && mouse_pos.0 < 146.0 && mouse_pos.1 > 6.0 && mouse_pos.1 < 22.0 {
-                squares = squares.saturating_sub(2);
+            if mouse_pos.0 > 136.0 && mouse_pos.0 < 152.0 && mouse_pos.1 > 6.0 && mouse_pos.1 < 22.0 {
+                squares_count = (squares_count - 2).max(4);
+                squares = Vec::with_capacity((squares_count * squares_count) as usize);
             }
         }
 
+        // Draw squares
+        let square_size = (mq::screen_height() - 100.0) / squares_count as f32;
+        let x_padding = (mq::screen_width() - square_size * squares_count as f32) / 2.0;
+        let y_padding = (mq::screen_height() - square_size * squares_count as f32) * (2.0 / 3.0);
+
+        // Draw filled squares
+        for y in 0..squares_count {
+            for x in 0..squares_count {
+                let index = xy_to_index(x, y, squares_count);
+                let square = squares.get(index).unwrap_or(&Square::Empty);
+                let color = match square {
+                    Square::Empty => mq::WHITE,
+                    Square::Sun => mq::YELLOW,
+                    Square::Moon => mq::BLUE,
+                };
+
+                mq::draw_rectangle(
+                    x_padding + x as f32 * square_size,
+                    y_padding + y as f32 * square_size,
+                    square_size,
+                    square_size,
+                    color,
+                );
+            }
+        }
+
+        // Draw grid
+        mq::draw_rectangle_lines(
+            x_padding,
+            y_padding,
+            square_size * squares_count as f32,
+            square_size * squares_count as f32,
+            5.0,
+            mq::BLACK,
+        );
+
+        for y in 0..squares_count {
+            for x in 0..squares_count {
+                mq::draw_rectangle_lines(
+                    x_padding + x as f32 * square_size,
+                    y_padding + y as f32 * square_size,
+                    square_size,
+                    square_size,
+                    3.0,
+                    mq::BLACK,
+                );
+            }
+        }
 
         // Debug - Draw mouse position
         let mouse_pos = mq::mouse_position();
@@ -57,9 +129,14 @@ async fn main() {
             mq::BLACK,
         );
 
-
-
-
+        // Debug - Draw fps
+        mq::draw_text(
+            &format!("FPS: {:.0}", mq::get_fps()),
+            10.0,
+            60.0,
+            16.0,
+            mq::BLACK,
+        );
 
         // Next frame
         mq::next_frame().await
