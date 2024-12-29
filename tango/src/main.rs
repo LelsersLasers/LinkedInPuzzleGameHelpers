bb4c6a7f4388fa701cb2af5f2c76242ca6c315c6
Lord-Lelsers <48894200+LelsersLasers@users.noreply.github.com>
2024-12-29 09:24:27 -0800

    Simple macroquad setup

--- tango/src/main.rs
+++ tango/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude as mq;
+
+fn window_conf() -> mq::Conf {
+    mq::Conf {
+        window_title: "Tango Solver".to_owned(),
+        window_width: 800,
+        window_height: 600,
+        ..Default::default()
+    }
+}
+
+#[macroquad::main(window_conf)]
+async fn main() {
+    loop {
+        mq::clear_background(mq::WHITE);
+        mq::draw_text("Hello, world!", 20.0, 20.0, 30.0, mq::BLACK);
+        mq::next_frame().await
+    }
 }
